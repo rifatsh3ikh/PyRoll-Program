80b725bc5740a611954144a3ebc2007a99063080
Rifat Sheikh <rifatsh3ikh@gmail.com>
2026-01-08 19:02:56 +0600

    update

---
+++ main.py
@@ -0,0 +1,34 @@
+import customtkinter as ctk
+import random
+
+# Set the appearance to dark mode for a "cool" look
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("green")
+
+root = ctk.CTk()
+root.iconbitmap("logo.ico") 
+root.geometry("500x400")
+root.title("PyRoller")
+root.geometry("400x400")
+
+def roll():
+    number = random.randint(1, 6)
+    
+    dice_faces = {
+        1: "⚀", 2: "⚁", 3: "⚂", 
+        4: "⚃", 5: "⚄", 6: "⚅"
+    }
+    dice_label.configure(text=dice_faces[number])
+    result_text.configure(text=f"You rolled {number}!")
+
+dice_label = ctk.CTkLabel(root, text="🎲", font=("Arial", 120))
+dice_label.pack(pady=40)
+
+result_text = ctk.CTkLabel(root, text="Roll Now!", font=("Arial", 20))
+result_text.pack(pady=10)
+
+roll_button = ctk.CTkButton(root, text="Roll Dice", command=roll, 
+                            corner_radius=10, hover_color="#246ab1")
+roll_button.pack(pady=20)
+root.mainloop()
+
